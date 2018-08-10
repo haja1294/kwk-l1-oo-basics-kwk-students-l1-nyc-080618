bbb41f299940310ba2304c44247253332a905df5
Haja Trawally <hajatrawally6@gmail.com>
2018-08-10 20:31:05 +0000

    automatically backed up by learn

--- table_group.rb
+++ table_group.rb
@@ -25,7 +25,7 @@ end
 
 Rochelle = Table_group.new("Game of Thrones", "Sushi", "3")
 Gabby = Table_group.new("Gossip Girl" , "Ham" , "6")
-adielle = Table_group.new("Gossip Girl" , "French Fries" , "3")
+Adielle = Table_group.new("Gossip Girl" , "French Fries" , "3")
 
 Rochelle.base_traits
 puts Rochelle.do_you_have_siblings
@@ -33,11 +33,11 @@ puts Rochelle.what_is_your_favorite_tv_show
 puts Rochelle.what_is_your_favorite_food
 
 Gabby.base_traits
-puts gabby.do_you_have_siblings
-puts gabby.what_is_your_favorite_food
-puts gabby.what_is_your_favorite_tv_show
+puts Gabby.do_you_have_siblings
+puts Gabby.what_is_your_favorite_food
+puts Gabby.what_is_your_favorite_tv_show
 
-adielle.base_traits
-puts adielle.do_you_have_siblings
-puts adielle.what_is_your_favorite_tv_show
-puts adielle.what_is_your_favorite_food
+Adielle.base_traits
+puts Adielle.do_you_have_siblings
+puts Adielle.what_is_your_favorite_tv_show
+puts Adielle.what_is_your_favorite_food
