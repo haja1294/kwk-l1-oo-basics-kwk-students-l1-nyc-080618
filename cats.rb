83a858a83249e2c7675ee06044ebb7954b16da1c
Haja Trawally <hajatrawally6@gmail.com>
2018-08-10 19:45:01 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -15,12 +15,21 @@ class Cats
  @fuzzy
   end
   
+  def is_it_mean 
+    @mean
+  end
+  
+  def is_it_cuddly
+   @cuddly 
+  end
   
 end 
 
 tiger = Cats.new("Oh , it's super snazzy","pretty fuzzy","super duper mean", "absoultely not")
 
-garfield= Cats.new("ewwwwww", "fuzzy","well....he kinda hates Mondays","ewwwwww")
+garfield = Cats.new("ewwwwww", "fuzzy","well....he kinda hates Mondays","ewwwwww")
 
-puts  garfield.is_it_cute
+puts garfield.is_it_cute
 puts garfield.is_it_fuzzy
+puts garfield.is_it_mean
+puts garfield.is_it_cuddly
