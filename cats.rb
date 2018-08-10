ea9f194a1d43f9cd5a097890597e291b530c50d2
Haja Trawally <hajatrawally6@gmail.com>
2018-08-10 19:36:28 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -1,1 +1,17 @@
-class Cat 
+class Cats
+ 
+  def initalize (cute,fuzzy,mean,cuddly )
+    @cute = cute 
+    @fuzzy = fuzzy 
+    @mean = mean 
+    @cuddly = cuddly 
+  end 
+  
+ def is_it_cute 
+ end
+  
+end 
+
+tiger = Cats.new("Oh , it's super snazzy","pretty fuzzy","super duper mean", "absoultely not")
+
+garfield= Cats.new("ewwwwww", "fuzzy","well....he kinda hates Mondays")
