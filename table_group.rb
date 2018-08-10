0aa7abd669c53250d529c53ca30a745894f3c49f
Haja Trawally <hajatrawally6@gmail.com>
2018-08-10 20:26:05 +0000

    automatically backed up by learn

--- table_group.rb
+++ table_group.rb
@@ -21,9 +21,4 @@ class Table_group
 @tv_show
   end
   
-  rochelle = Table_group.new("Game of Thrones", "Sushi", "3")
-  gabby = Table_group.new("Gossip Girl" , "Ham" , "6")
-  adielle = Table_group.new("Gossip Girl" , "French Fries" , "3")
-
-  
 end
